5c6d0bcfe204015312ec07c494ec24c7e9de6f98
tehandyb <tehandyb@gmail.com>
2017-06-10 21:51:02 -0400

    moved the initial boilerplate, and took code from react-choropleth

---
+++ rollup.demo.config.js
@@ -0,0 +1,43 @@
+import babel from 'rollup-plugin-babel'
+import commonjs from 'rollup-plugin-commonjs'
+import nodeResolve from 'rollup-plugin-node-resolve'
+import uglify from 'rollup-plugin-uglify'
+import replace from 'rollup-plugin-replace'
+import serve from 'rollup-plugin-serve'
+import livereload from 'rollup-plugin-livereload'
+import postcss from 'rollup-plugin-postcss'
+
+// PostCSS plugins
+import simplevars from 'postcss-simple-vars'
+import nested from 'postcss-nested'
+import cssnext from 'postcss-cssnext'
+
+// Bundle using an entry point as if it was a real app using the choropleth
+export default {
+  entry: 'demo/entry.js',
+  dest: 'demo/demo-bundle.js',
+  format: 'umd',
+  plugins: [
+    serve('demo'),
+    livereload({
+      watch: 'demo'
+    }),
+    babel({
+      exclude: 'node_modules/**'
+    }),
+    nodeResolve({
+      jsnext: true
+    }),
+    commonjs({
+      include: ['node_modules/**', 'src/**', 'demo/**'],
+      namedExports: {
+        'node_modules/react/react.js': ['Component', 'PureComponent'],
+        'node_modules/immutable/dist/immutable.js': ['fromJS']
+      }
+    }),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('development')
+    })
+  ],
+  sourceMap: true,
+}
---
+++ rollup.dist.config.js
@@ -0,0 +1,36 @@
+import babel from 'rollup-plugin-babel'
+import commonjs from 'rollup-plugin-commonjs'
+import nodeResolve from 'rollup-plugin-node-resolve'
+import replace from 'rollup-plugin-replace'
+import postcss from 'rollup-plugin-postcss'
+
+// PostCSS plugins
+import simplevars from 'postcss-simple-vars'
+import nested from 'postcss-nested'
+import cssnext from 'postcss-cssnext'
+
+export default {
+  entry: 'src/with-tooltips.js',
+  dest: 'dist/with-tooltips.js',
+  format: 'umd',
+  moduleName: 'ReactWithTooltips',
+  plugins: [
+    babel({
+      exclude: 'node-modules/**'
+    }),
+    nodeResolve({
+      jsnext: true
+    }),
+    commonjs({
+      include: 'node_modules/**',
+      namedExports: {
+        'node_modules/immutable/dist/immutable.js': ['fromJS']
+      }
+    }),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+  ],
+  // Don't bloat our bundle with React, the parent application will provide it
+  external: ['react', 'react-dom']
+}
